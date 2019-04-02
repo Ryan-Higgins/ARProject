e9c2d35e13932f8bcad86f03098e737d646c5a00
Ryan-Higgins <c16381886@mydit.ie>
2019-04-02 14:28:07 +0100

    Camera Switching between AR and main

---
+++ ARProject/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    private GameObject mainCamera;
+    public GameObject aRCamera;
+    bool switchCheck = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchCamera()
+    {
+        //mainCamera.SetActive(false);
+        switchCheck = !switchCheck;
+        aRCamera.SetActive(switchCheck);
+    }
+}
