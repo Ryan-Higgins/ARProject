44cf1df8869ea5683de0b07dcd6c2842427243c7
Samuel Hegner <C16439262@mydit.ie>
2019-04-03 14:26:51 +0100

    messages work when iteams are clicked

---
+++ ARProject/Assets/PopulatePopUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PopulatePopUp : MonoBehaviour
+{
+    public Sprite[] targets;
+
+    public Image img;
+    public TextMeshProUGUI text;
+
+
+    public void SetPopUp(int numberOfSprite, string popUpText)
+    {
+        img.preserveAspect = true;
+
+        img.sprite = targets[numberOfSprite];
+        text.text = popUpText;
+        gameObject.SetActive(true);
+    }
+
+    public void ClearPopUp(){
+        img.sprite = null;
+        text.text = "";
+    }
+}
